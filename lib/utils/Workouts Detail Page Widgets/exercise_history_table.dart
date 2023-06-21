db3042e17d3018e1db8d8a6d09214b93fbb8586e
memrec45 <65484287+memrec45@users.noreply.github.com>
2023-06-21 17:56:28 +0300

    Checkpoint

--- lib/utils/Workouts Detail Page Widgets/exercise_history_table.dart
+++ lib/utils/Workouts Detail Page Widgets/exercise_history_table.dart
@@ -15,41 +15,29 @@ class ExerciseHistoryTable extends StatefulWidget {
 }
 
 class _ExerciseHistoryTableState extends State<ExerciseHistoryTable> {
-  Visibility moreDetail(bool showMore, List data, String entry) {
-    return Visibility(
-      visible: showMore,
-      child: Column(
-        children: [
-          ...data.reversed.map(
-            (e) {
-              return Padding(
-                padding: const EdgeInsets.only(top: 8.0),
-                child: Text(
-                  e[entry].toString() + (entry == "weight" ? " Kg" : ""),
-                  style: context.textTheme.labelSmall!.copyWith(fontSize: 12),
-                ),
-              );
-            },
-          )
-        ],
-      ),
-    );
-  }
-
   late final List<QueryDocumentSnapshot<Map<String, dynamic>>> dataList;
+
   @override
   void initState() {
     super.initState();
     dataList = widget.dataList;
   }
 
-  bool _showMore = false;
   @override
   Widget build(BuildContext context) {
     int index = 0;
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: SingleChildScrollView(
+        ////
+        /// FIXME:
+        /// !Two option :
+        /// ? - 1 : Wrap the elements in [titleRow] with a [Row]. There will one column inside the first [TableRow].
+        ///         Wrap the elements of [DataRows] with a [Row] and wrap that [Row] with a [StateFulBuilder].
+        ///         Instead of [FlexColumnWidth] wrap the elements inside the [Row] with [Flexible]
+        /// ? - 2 : THERE IS NO SECOND OPTION.
+        ////
+
         child: Table(
           columnWidths: const {
             0: FlexColumnWidth(3),
@@ -100,108 +88,12 @@ class _ExerciseHistoryTableState extends State<ExerciseHistoryTable> {
                 final Map lastData = e.data()["data"].last;
                 final List data = e.data()["data"];
                 data.removeLast();
-                return DataRows(context,
-                    lastData: lastData, data: data, date: date);
-
-                //   TableRow(
-                //     children: [
-                //       ///* date
-                //       Padding(
-                //         padding: const EdgeInsets.only(top: 2.0),
-                //         child: Column(
-                //           children: [
-                //             Row(
-                //               children: [
-                //                 /// See More Details Button
-                //                 GestureDetector(
-                //                   onTap: () => setState(
-                //                     () {
-                //                       _showMore = !_showMore;
-                //                     },
-                //                   ),
-                //                   child: Icon(
-                //                     color: AppColors.kButtonColor,
-                //                     _showMore
-                //                         ? Icons.keyboard_arrow_up_rounded
-                //                         : Icons.keyboard_arrow_down_rounded,
-                //                   ),
-                //                 ),
-                //                 Text(
-                //                   date.toString(),
-                //                   style: context.textTheme.labelSmall!
-                //                       .copyWith(fontSize: 12),
-                //                 ),
-                //               ],
-                //             ),
-                //             Visibility(
-                //               visible: _showMore,
-                //               child: Column(
-                //                 children: List.generate(
-                //                   data.length,
-                //                   (index) => Padding(
-                //                     padding: const EdgeInsets.only(top: 8.0),
-                //                     child: Text(
-                //                       (data.length - index).toString(),
-                //                       style: context.textTheme.labelSmall!
-                //                           .copyWith(fontWeight: FontWeight.bold),
-                //                     ),
-                //                   ),
-                //                 ),
-                //               ),
-                //             )
-                //           ],
-                //         ),
-                //       ),
-
-                //       ///* Rep
-                //       Padding(
-                //         padding: const EdgeInsets.only(top: 8.0),
-                //         child: Column(
-                //           crossAxisAlignment: CrossAxisAlignment.start,
-                //           children: [
-                //             Text(
-                //               lastData["rep"].toString(),
-                //               style: context.textTheme.labelSmall!
-                //                   .copyWith(fontSize: 12),
-                //             ),
-                //             moreDetail(_showMore, data, "rep"),
-                //           ],
-                //         ),
-                //       ),
-
-                //       ///* Weight
-                //       Padding(
-                //         padding: const EdgeInsets.only(top: 8.0),
-                //         child: Column(
-                //           crossAxisAlignment: CrossAxisAlignment.start,
-                //           children: [
-                //             Text(
-                //               "${lastData["weight"]} Kg",
-                //               style: context.textTheme.labelSmall!
-                //                   .copyWith(fontSize: 12),
-                //             ),
-                //             moreDetail(_showMore, data, "weight"),
-                //           ],
-                //         ),
-                //       ),
-
-                //       ///* Rpe
-                //       Padding(
-                //         padding: const EdgeInsets.only(top: 8.0),
-                //         child: Column(
-                //           crossAxisAlignment: CrossAxisAlignment.start,
-                //           children: [
-                //             Text(
-                //               lastData["rpe"].toString(),
-                //               style: context.textTheme.labelSmall!
-                //                   .copyWith(fontSize: 12),
-                //             ),
-                //             moreDetail(_showMore, data, "rpe"),
-                //           ],
-                //         ),
-                //       ),
-                //     ],
-                //   );
+                return DataRows(
+                  context,
+                  lastData: lastData,
+                  data: data,
+                  date: date,
+                );
               },
             ).toList(),
           ],
@@ -276,24 +168,24 @@ class DataRows extends TableRow {
                   ),
                 ],
               ),
-              moreDetail(_showMore, data, "rpe"),
+              // moreDetail(_showMore, data, "rpe"),
 
-              // Visibility(
-              //   visible: _showMore,
-              //   child: Column(
-              //     children: List.generate(
-              //       data.length,
-              //       (index) => Padding(
-              //         padding: const EdgeInsets.only(top: 8.0),
-              //         child: Text(
-              //           (data.length - index).toString(),
-              //           style: context.textTheme.labelSmall!
-              //               .copyWith(fontWeight: FontWeight.bold),
-              //         ),
-              //       ),
-              //     ),
-              //   ),
-              // )
+              Visibility(
+                visible: _showMore,
+                child: Column(
+                  children: List.generate(
+                    data.length,
+                    (index) => Padding(
+                      padding: const EdgeInsets.only(top: 8.0),
+                      child: Text(
+                        (data.length - index).toString(),
+                        style: context.textTheme.labelSmall!
+                            .copyWith(fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                ),
+              )
             ],
           ),
         ),
@@ -344,152 +236,5 @@ class DataRows extends TableRow {
         ),
       ),
     ];
-  }
-}
-
-class DataRowsa extends StatefulWidget {
-  const DataRowsa(
-      {super.key,
-      required this.lastData,
-      required this.data,
-      required this.date});
-
-  final Map lastData;
-  final List data;
-  final String date;
-  @override
-  State<DataRowsa> createState() => _DataRowsaState();
-}
-
-class _DataRowsaState extends State<DataRowsa> {
-  late final Map lastData;
-  late final List data;
-  late final String date;
-
-  @override
-  void initState() {
-    super.initState();
-    lastData = widget.lastData;
-    data = widget.data;
-    date = widget.date;
-  }
-
-  bool _showMore = false;
-  Widget moreDetail(bool showMore, List data, String key) {
-    return Visibility(
-      visible: showMore,
-      child: Column(
-        children: [
-          ...data.reversed.map((e) {
-            return Padding(
-              padding: const EdgeInsets.only(top: 8.0),
-              child: Text(
-                e[key] + (key == "weight" ? " Kg" : ""),
-                style: context.textTheme.labelSmall!.copyWith(fontSize: 12),
-              ),
-            );
-          })
-        ],
-      ),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: [
-        ///* date
-        Padding(
-          padding: const EdgeInsets.only(top: 2.0),
-          child: Column(
-            children: [
-              Row(
-                children: [
-                  /// See More Details Button
-                  GestureDetector(
-                    onTap: () => setState(
-                      () {
-                        _showMore = !_showMore;
-                      },
-                    ),
-                    child: Icon(
-                      color: AppColors.kButtonColor,
-                      _showMore
-                          ? Icons.keyboard_arrow_up_rounded
-                          : Icons.keyboard_arrow_down_rounded,
-                    ),
-                  ),
-                  Text(
-                    date.toString(),
-                    style: context.textTheme.labelSmall!.copyWith(fontSize: 12),
-                  ),
-                ],
-              ),
-              Visibility(
-                visible: _showMore,
-                child: Column(
-                  children: List.generate(
-                    data.length,
-                    (index) => Padding(
-                      padding: const EdgeInsets.only(top: 8.0),
-                      child: Text(
-                        (data.length - index).toString(),
-                        style: context.textTheme.labelSmall!
-                            .copyWith(fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                  ),
-                ),
-              )
-            ],
-          ),
-        ),
-
-        ///* Rep
-        Padding(
-          padding: const EdgeInsets.only(top: 8.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                lastData["rep"].toString(),
-                style: context.textTheme.labelSmall!.copyWith(fontSize: 12),
-              ),
-              moreDetail(_showMore, data, "rep"),
-            ],
-          ),
-        ),
-
-        ///* Weight
-        Padding(
-          padding: const EdgeInsets.only(top: 8.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                "${lastData["weight"]} Kg",
-                style: context.textTheme.labelSmall!.copyWith(fontSize: 12),
-              ),
-              moreDetail(_showMore, data, "weight"),
-            ],
-          ),
-        ),
-
-        ///* Rpe
-        Padding(
-          padding: const EdgeInsets.only(top: 8.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                lastData["rpe"].toString(),
-                style: context.textTheme.labelSmall!.copyWith(fontSize: 12),
-              ),
-              moreDetail(_showMore, data, "rpe"),
-            ],
-          ),
-        ),
-      ],
-    );
   }
 }
